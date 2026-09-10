2669a16c8c0797d679bf7260daca90a30717fbdd
malker15 <144722245+malker15@users.noreply.github.com>
2026-09-10 20:36:16 +0200

    Use visual 5-25 and 5xC trainer in Stage IV

--- app/stage4/Stage4StudyPanel.tsx
+++ app/stage4/Stage4StudyPanel.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import CffGridTrainer from "./CffGridTrainer";
 import MedevacTrainer from "./MedevacTrainer";
+import FiveTwentyFiveTrainer from "./FiveTwentyFiveTrainer";
 import { extraShootingMaterial, stage4StudyMaterials, type Stage4StudyMaterial } from "./driveStudyMaterials";
 
 function StudyCard({ material }: { material: Stage4StudyMaterial }) {
@@ -80,6 +81,7 @@ function StudyCard({ material }: { material: Stage4StudyMaterial }) {
 }
 
 export function Stage4StudyPanel({ stationId }: { stationId: string }) {
+  if (stationId === "5-25-5xc") return <FiveTwentyFiveTrainer />;
   if (stationId === "cff") return <CffGridTrainer />;
   if (stationId === "casualty") return <MedevacTrainer />;
   const material = stage4StudyMaterials[stationId];
